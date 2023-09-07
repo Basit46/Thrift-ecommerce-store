82924ad69d7af331abe7c10070bf86baf7430548
Basit46 <Hassanbasit1990@gmail.com>
2023-09-07 17:04:05 +0100

    Added changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
         />
       </div>
       <div className="relative w-full h-full backdrop-blur-[5px] xl:backdrop-blur-none text-white px-[5px] xl:px-[60px] pt-[100px] vsm:pt-[50px] xl:pt-0">
-        <h1 className="font-braahOne text-center text-[3rem] vsm:text-[3.5rem] sm:text-[5rem] xmd:text-[7rem] xl:text-[10rem] leading-[1]">
+        <h1 className="font-braahOne text-center vsm:text-left text-[3rem] vsm:text-[3.5rem] sm:text-[5rem] xmd:text-[7rem] xl:text-[10rem] leading-[1]">
           THRIFT * STORE
         </h1>
         <p className="mt-[20px] xl:mt-0 text-[1rem] vsm:text-[1.2rem] pl-[20px] font-medium text-center xl:text-left mx-auto xl:mx-0 w-[90%] xmd:w-[70%] xl:w-[40%]">
